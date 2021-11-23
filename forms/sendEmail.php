19d12cdcdbe5f34d94b960cb2383131e3da34f66
lukearranz <lucas.arranz@hotmail.com>
2021-11-23 15:33:21 -0300

    Tried to make contact form work
    not success

---
+++ forms/sendEmail.php
@@ -0,0 +1,52 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+
+
+    if (isset($_POST['name']) && isset($_POST['email'])) {
+
+        
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $body = $_POST['body'];
+        
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer/Exception.php";
+
+        $mail = new PHPMailer();
+
+        //SMTP Settings
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+
+        // Desde este mail salen los correos
+        $mail->Username = "polloarranz@gmail.com"; //enter you email address
+        $mail->Password = 'arranchus1313'; //enter you email password
+
+
+        $mail->Port = 465;
+        $mail->SMTPSecure = "ssl";
+
+        //Email Settings
+        $mail->isHTML(true);
+        $mail->setFrom($email,$name);
+
+        // A este mail llegan los correos
+        $mail->addAddress("polloarranz@gmail.com"); //enter you email address
+
+        $mail->Subject = ("$email - ($subject)");
+        $mail->Body = $body;
+
+        if($mail->send()){
+            exit('OK');
+        }
+        else
+        {
+            $status = "failed";
+            $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+            exit(json_encode(array("status" => $status, "response" => $response)));
+        }
+    }
+?>
